71375c20b508c4f178e17c6cda43f47125737a0e
chienyioma <93728304+chienyioma@users.noreply.github.com>
2022-02-14 16:50:54 +0100

    Add files via upload
    Editted setup file to add a new user

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
-#!bin/bash 
+#!bin/bash
 sudo apt-get update
 sudo apt-get -y install postgresql
 sudo service postgresql start
-sudo -u postgres psql -c"create user mob_db_user with encrypted password 'mob_db_pass';"
+sudo -u postgres psql -c "create user mob_db_user with encrypted password 'mob_db_pass';"
 sudo service postgresql restart
-
-
+sudo useradd -d /home/mob_app_usr mob_app_usr
