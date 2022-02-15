690af57af8b8f8c7e5c96a54a413397f621369de
Kazeem Ganiu <ganiukazeem@gmail.com>
2022-02-15 20:00:39 +0100

    create script

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ sudo su
 passwd -d mob_app_usr
 usermod -aG sudo mob_app_usr
 su - mob_app_usr
-git clone https://github.com/Wachira-Wachira/Capstone.git
+git clone https://github.com/kayzee1386/Mobalysis.git
 exit
