c6e3ff2ea762f10fa87fcffc0e42c252be1686c7
chienyioma <93728304+chienyioma@users.noreply.github.com>
2022-02-16 17:03:18 +0100

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,10 +3,11 @@ sudo su
 passwd -d mob_app_usr
 usermod -aG sudo mob_app_usr
 su - mob_app_usr
-nano /home/mob_app_usr/.bashrc      
+sudo su mob_app_usr
+nano ~/. bashrc
 export DBNAME=mobalytics
 export DBUSER=mob_db_user
 export DBPASS=mob_db_pass
 export DBHOST=localhost
 export DBPORT=5432
-source .bashrc
+source ~/. bashrc
