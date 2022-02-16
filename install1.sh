0c638ea62bc17264565a114141c53c16c4e820ba
chienyioma <93728304+chienyioma@users.noreply.github.com>
2022-02-16 11:57:15 +0100

    Add files via upload

---
+++ install1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo su
+passwd -d mob_app_usr
+usermod -aG sudo mob_app_usr
+su - mob_app_usr
+nano /home/mob_app_usr/.bashrc      
+export DBNAME=mobalytics
+export DBUSER=mob_db_user
+export DBPASS=mob_db_pass
+export DBHOST=localhost
+export DBPORT=5432
+source .bashrc
