ae0e5fb887dba3c1d5101224e837fc74181e9282
chienyioma <93728304+chienyioma@users.noreply.github.com>
2022-02-16 17:08:54 +0100

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,6 +3,7 @@ sudo su
 passwd -d mob_app_usr
 usermod -aG sudo mob_app_usr
 su - mob_app_usr
+git clone https://github.com/Wachira-Wachira/Capstone.git
 sudo su mob_app_usr
 nano ~/. bashrc
 export DBNAME=mobalytics
